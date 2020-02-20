2b44598c845d4ea80dd46f3009ddaf7bb4560f20
dashakon <dashakondrattenko@gmail.com>
2020-02-19 18:03:09 -0800

    adding changes

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,18 +4,13 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // var map = L.mapbox.map('mapid')
 //     .setView([40, -74.50], 9)
 //     .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
-// We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}.png?access_token={accessToken}', {
-// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-// 	maxZoom: 18,
-// 	id: 'mapbox.streets',
-// 	accessToken: API_KEY
-// });
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
-
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//We create the tile layer that will be the background of our map.
+L.circleMarker([34.0522, -118.2437], {
+	radius: 100,
+	color: 'black',
+	fillColor: '#ffffa1'
+ }).addTo(map);
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	id: 'mapbox.streets',
@@ -23,6 +18,20 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// We create the tile layer that will be the background of our map.
+//  Add a marker to the map for Los Angeles, California.
+// L.circle([34.0522, -118.2437], {
+// 	radius: 100
+//  }).addTo(map);
+// let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+// 	maxZoom: 18,
+// 	id: 'mapbox.streets',
+// 	accessToken: API_KEY
+// });
+// // Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
 
 // L.map('mapid')
 //   .setView([38.8929, -77.0252], 14)
