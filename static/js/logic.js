e2314b7aadd42e67a55c3eed7fb7b8cb2b061ca2
dashakon <dashakondrattenko@gmail.com>
2020-02-20 22:34:03 -0800

    addign files

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,32 +1,32 @@
-console.log("working", API_KEY);
+console.log("working");
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
-// var map = L.mapbox.map('mapid')
-//     .setView([40, -74.50], 9)
-//     .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
-// We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}.png?access_token={accessToken}', {
-// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-// 	maxZoom: 18,
-// 	id: 'mapbox.streets',
-// 	accessToken: API_KEY
-// });
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	id: 'mapbox.streets',
 	accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
-
-// L.map('mapid')
-//   .setView([38.8929, -77.0252], 14)
-//   .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
-
-
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+let baseMaps = {
+	Street: streets,
+	Dark: dark
+  };
+  let map = L.map("mapid", {
+	center: [40.7, -94.5],
+	zoom: 4
+  });
+  L.control.layers(baseMaps).addTo(map);
+let airportData = 'https://raw.githubusercontent.com/AlenaKB/Mapping_Earthquakes/master/majorAirports.json';
+d3.json(airportData).then(function(data) {
+	console.log(data);
+	L.geoJson(data, {
+		onEachFeature: function(feature, layer) {
+			layer.bindPopup("<h3>" + "Airport Code: " + feature.properties.faa + "</h3>"+ "<hr>" + "<h3>" + "Airport Name: "  + feature.properties.name + "</h3>")
+		}
+	}).addTo(map);
+});
 
