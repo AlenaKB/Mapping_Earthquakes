8bf611f637c09a6831611b979fe39ab0cc8da2aa
dashakon <dashakondrattenko@gmail.com>
2020-02-21 15:05:39 -0800

    adding file

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,31 +1,31 @@
 console.log("working");
-// Create the map object with a center and zoom level.
-
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
 let baseMaps = {
-	Street: streets,
-	Dark: dark
+	"Streets": streets,
+	"Satellite Streets": satelliteStreets
   };
   let map = L.map("mapid", {
-	center: [40.7, -94.5],
-	zoom: 4
+	center: [43.7, -79.3],
+	color:'yellow',
+	zoom: 11,
+	weight: 1
+
   });
   L.control.layers(baseMaps).addTo(map);
-let airportData = 'https://raw.githubusercontent.com/AlenaKB/Mapping_Earthquakes/master/majorAirports.json';
-d3.json(airportData).then(function(data) {
+let torontoHoods = 'https://raw.githubusercontent.com/AlenaKB/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json';
+d3.json(torontoHoods).then(function(data) {
 	console.log(data);
 	L.geoJson(data, {
 		onEachFeature: function(feature, layer) {
-			layer.bindPopup("<h3>" + "Airport Code: " + feature.properties.faa + "</h3>"+ "<hr>" + "<h3>" + "Airport Name: "  + feature.properties.name + "</h3>")
-		}
-	}).addTo(map);
+        layer.bindPopup("<h3>" + "Neighborhood: " + feature.properties.AREA_NAME + "</h3>")
+}}).addTo(map);
 });
